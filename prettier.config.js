4ee79040a230a1402008e6ebbeffa06a54911e75
Chi's <1512984584@qq.com>
2023-04-04 10:43:27 +0800

    chore: add prettier&eslint configure

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  arrowParens: 'avoid',
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  htmlWhitespaceSensitivity: 'ignore', // ignore css
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+}
