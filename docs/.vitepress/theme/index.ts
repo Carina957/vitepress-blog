cddd77594229dc40317e5415e82aa06606d8c434
Chi's <1512984584@qq.com>
2023-05-17 19:17:44 +0800

    remove: remove useless comments

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,17 +1,7 @@
-// import { h } from 'vue'
 import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import './styles/demo.css'
-import DemoContainer from './components/DemoContainer.vue'
 
 export default {
   ...Theme,
-  // Layout() {
-  //   return h(Theme.Layout, null, {
-  //     'demo-container': () => h(DemoContainer),
-  //   })
-  // },
-  enhanceApp({ app }) {
-    app.component('SvgImage', DemoContainer)
-  },
 }
