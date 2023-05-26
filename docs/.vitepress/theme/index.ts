754601f8b4c1a48df5d96f7cae542d37721b6f82
Chi's <1512984584@qq.com>
2023-05-26 11:52:58 +0800

    style: sliding-enter-animation & scrollbar style

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,8 @@
 import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import './styles/demo.css'
+import './styles/scrollbar.css'
+import './styles/sliding-enter-animation.css'
 
 export default {
   ...Theme,
