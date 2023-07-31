dc900a6adaab3406ad20d4b4ee8e788cc808eee6
Chi's <1512984584@qq.com>
2023-07-31 11:09:59 +0800

    feat: add medium-zoom

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,9 +1,27 @@
+import { nextTick, watch, onMounted } from 'vue'
+import { useRoute } from 'vitepress'
+
 import Theme from 'vitepress/theme'
 import './styles/vars.css'
 import './styles/demo.css'
 import './styles/scrollbar.css'
 import './styles/sliding-enter-animation.css'
+import './styles/medium-zoom.css'
+
+import mediumZoom from 'medium-zoom'
 
 export default {
   ...Theme,
+  setup() {
+    const route = useRoute()
+    const initMediumZoom = () =>
+      mediumZoom('.content img', { background: 'var(--vp-c-bg)' })
+
+    onMounted(() => initMediumZoom())
+
+    watch(
+      () => route.path,
+      () => nextTick(() => initMediumZoom())
+    )
+  },
 }
